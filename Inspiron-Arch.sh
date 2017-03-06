ce3bc0f207fa591a5730b57dd53782d8978188c5
manoeldesouza <manoel.desouza@outlook.com.br>
2017-03-06 18:14:06 -0300

    Add files via upload

--- Inspiron-Arch.sh
+++ Inspiron-Arch.sh
@@ -49,6 +49,8 @@ pacstrap /mnt base base-devel
 genfstab -U /mnt >> /mnt/etc/fstab
 nano /mnt/etc/fstab
 # /dev/sda2  none swap defaults 0 0
+# add discard to /
+
 arch-chroot /mnt
 
 
@@ -87,6 +89,10 @@ nano /etc/vconsole.conf
 pacman -S git wget git reflector mc vim lynx 
 reflector -c BR
 nano /etc/pacman.d/mirrorlist
+# uncomment Multilib
+
+sudo pacman -S p7zip unace unrar zip unzip sharutils uudeview arj cabextract file-roller
+
 
 
 nano /etc/locale.gen
@@ -110,11 +116,12 @@ date
 
 
 echo "1.7 Network setup:"
-echo Inspiron > /etc/hostname
+hostnamectl set-hostname Inspiron
+
 nano /etc/hosts
 # 127.0.1.1     localhost.localdomain   Inspiron
 
-pacman -S base-devel fakeroot jshon expac grep sed curl tmux bash-completion openssh linux-headers linux-lts linux-lts-headers wpa_supplicant wireless_tools networkmanager iw wpa_supplicant dialog
+pacman -S base-devel fakeroot jshon expac grep sed curl tmux bash-completion openssh linux-headers linux-lts linux-lts-headers wpa_supplicant wireless_tools networkmanager network-manager-applet iw wpa_supplicant wpa_actiond dialog
 
 systemctl enable sshd.service
 systemctl enable NetworkManager.service
@@ -160,56 +167,61 @@ SigLevel = Never
 Server = http://repo.archlinux.fr/$arch
 sudo pacman -Sy yaourt
 
+
 mkdir ~/Downloads
 cd ~/Downloads
-git clone https://github.com/erikdubois/archcinnamon
-git clone https://github.com/erikdubois/Aureola
+#git clone https://github.com/erikdubois/archcinnamon
+#git clone https://github.com/erikdubois/Aureola
 git clone https://github.com/manoeldesouza/Cookbook
 
-sudo systemctl stop dhcpcd.service
-sudo systemctl start sshd.service
-sudo systemctl start wpa_supplicant.service
-sudo systemctl start NetworkManager.service
-sudo systemctl start bluetooth.service
+#sudo systemctl stop dhcpcd.service
+#sudo systemctl start sshd.service
+#sudo systemctl start wpa_supplicant.service
+#sudo systemctl start NetworkManager.service
+#sudo systemctl start bluetooth.service
+
+sudo pacman -S xf86-input-libinput xorg-server xorg-xinit xorg-server-utils mesa 
+sudo pacman -S xorg-twm xorg-xclock xterm
+sudo pacman -S xf86-video-intel lib32-intel-dri lib32-mesa lib32-libgl
+
+sudo pacman -S alsa-utils pulseaudio pulseaudio-bluetooth
 
 
-sudo pacman -S p7zip unace unrar zip unzip sharutils uudeview arj cabextract file-roller
-
-pacman -S alsa-utils pulseaudio pulseaudio-bluetooth
-pacman -S xorg-server xorg-xinit xorg-server-utils xorg-twm xorg-xclock xterm
-pacman -S mesa
 #pacman -S nvidia nvidia-libgl xorg-xrandr nvidia-settings
-pacman -S xorg-xrandr
+#pacman -S xorg-xrandr
+
+#sudo pacman -S pulseaudio pulseaudio-alsa pavucontrol alsa-utils alsa-plugins alsa-lib alsa-firmware
 
 
-sudo pacman -S pulseaudio pulseaudio-alsa pavucontrol alsa-utils alsa-plugins alsa-lib alsa-firmware
-
-sudo pacman -S xorg-server xorg-utils xorg-xinit xorg-twm xorg-xclock xorg-server-utils mesa xterm
-sudo pacman -S xf86-video-intel 
-sudo pacman -S xf86-input-synaptics
-sudo pacman -S cinnamon nemo-fileroller network-manager-applet network-manager-applet gnome-keyring
-
-sudo pacman -S xf86-input-libinput
-sudo systemctl enable NetworkManager.service
-sudo systemctl enable gdm.service
+sudo pacman -S cinnamon nemo-fileroller gnome-keyring
 
 # sudo pacman –S mdm-display-manager
 # sudo systemctl start mdm-service
 # sudo systemctl enable mdm-service
 
-sudo pacman -S cups cups-pdf ghostscript gsfonts libcups hplip system-config-printer
-sudo systemctl enable org.cups.cupsd.service
-sudo systemctl start org.cups.cupsd.service
+# sudo pacman –S mdm-display-manager
+#sudo systemctl start gdm.service
+#sudo systemctl enable gdm.service
 
 
+
+#sudo pacman -S cups cups-pdf ghostscript gsfonts libcups hplip system-config-printer
+#sudo systemctl enable org.cups.cupsd.service
+#sudo systemctl start org.cups.cupsd.service
+
+
+yaourt -S pamac-aur
 
 sudo pacman -S gnome-terminal gedit gnome-system-monitor gnome-font-viewer gnome-screenshot galculator geary gparted net-tools gpick grsync hardinfo hddtemp hexchat htop nemo nemo-share nemo-fileroller noto-fonts gthumb evince gvfs-afc gvfs-goa gvfs-google gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb
+
 sudo pacman -S a52dec faac faad2 flac jasper lame libdca libdv libmad libmpeg2 libtheora libvorbis libxv wavpack x264 xvidcore 
+
 sudo pacman -S gst-plugins-good gst-plugins-bad gst-plugins-base gst-plugins-ugly gstreamer
 
 sudo pacman -S firefox flashplugin chromium clementine conky darktable dconf-editor filezilla galculator geany gimp gksu glances inkscape inxi lm_sensors lsb-release meld mlocate mpv notify-osd numlockx openshot plank polkit-gnome redshift ristretto sane screenfetch scrot shotwell simple-scan simplescreenrecorder smplayer sysstat terminator transmission-cli transmission-gtk tumbler variety vnstat unclutter libreoffice vlc rhythmbox dropbox smbclient samba
 
 sudo pacman -S faenza-icon-theme faience-icon-theme arc-icon-theme arc-gtk-theme 
+
 
 sudo pacman -S virtualbox virtualbox-host-modules virtualbox-guest-iso
 sudo gpasswd -a $USER vboxusers
@@ -223,4 +235,13 @@ sudo nano /etc/modules-load.d/virtualbox.conf
 
 yaourt -S ttf-ms-fonts 
 yaourt -S mint-x-theme mint-y-theme moka-icon-theme faba-icon-theme 
-yaourt -S pamac-aur
+
+
+
+
+
+
+
+#  http://turlucode.com/arch-linux-install-guide-step-2-desktop-environment-installation/
+
+
